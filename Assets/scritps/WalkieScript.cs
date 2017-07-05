4bb75fac5036af95e3117a942b44a2bed12c98ce
Unknown <andrewcarrick710@gmail.com>
2017-07-05 14:16:01 -0500

    i (aka moogle) did it!!

---
+++ Assets/scritps/WalkieScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkieScript : MonoBehaviour {
+	public float speed;
+	CharacterController jimmy;
+	// Use this for initialization
+	void Start () {
+		jimmy = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal"); //this gives the joystick left and right input
+		float vertical = Input.GetAxis("Vertical");
+		jimmy.Move(transform.forward *Time.deltaTime *vertical *speed);
+		jimmy.Move (transform.right * Time.deltaTime * horizontal * speed);
+		transform.Rotate (0, Input.GetAxis ("Mouse X") * Time.deltaTime * 80f, 0);
+	}
+}

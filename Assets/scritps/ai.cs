9af56119c5351dc742987c7d09db5c245c741000
Unknown <andrewcarrick710@gmail.com>
2017-07-06 14:24:49 -0500

    cfxhcfhcdgfg

---
+++ Assets/scritps/ai.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai : MonoBehaviour {
+	public float walkingdistance = 10.0f;
+	public Transform player;
+	public float smoothtime = 10.0f;
+		private Vector3 smoothvelocity = Vector3.zero * 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player);
+
+		float distance = Vector3.Distance (transform.position, player.position);
+
+		if (distance < walkingdistance)
+			transform.position = Vector3.SmoothDamp (transform.position, player.position, ref smoothvelocity, smoothtime);
+	}
+}
